08963df6872d6273575af5684bf11ee5745bb479
SergieCode <sergiosnsdk@gmail.com>
2023-07-01 14:34:13 -0300

    Aplicación de peliculas para el curso de react

---
+++ src/BuscadorPeliculas.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+
+export const BuscadorPeliculas = () => {
+
+  const urlBase = 'https://api.themoviedb.org/3/search/movie'
+  const API_KEY = 'acf5472e45e436d5026bfd4c5e792981'
+
+  const [busqueda, setBusqueda] = useState('')
+  const [peliculas, setPeliculas] = useState([])
+
+  const handleInputChange = (e) => {
+    setBusqueda(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetchPeliculas()
+  }
+
+  const fetchPeliculas = async () => {
+    try {
+      const response = await fetch(`${urlBase}?query=${busqueda}&api_key=${API_KEY}`)
+      const data = await response.json()
+      console.log(data.results)
+      setPeliculas(data.results)
+    } catch (error) {
+      console.error('Ha ocurrido un error: ', error)
+    }
+  }
+
+  return (
+    <div className="container">
+
+      <h1 className="title">Buscador de Películas</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Escribí una película"
+          value={busqueda}
+          onChange={handleInputChange}
+        />
+        <button type="submit" className="search-button">Buscar</button>
+      </form>
+
+
+      <div className="movie-list">
+        {peliculas.map((pelicula) => (
+          <div key={pelicula.id} className="movie-card">
+            <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} alt={pelicula.title} />
+            <h2>{pelicula.title}</h2>
+            <p>{pelicula.overview}</p>
+          </div>
+
+        ))}
+
+      </div>
+
+    </div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BuscadorPeliculas } from './BuscadorPeliculas'
+import './styles/buscador.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BuscadorPeliculas />
+  </React.StrictMode>,
+)
